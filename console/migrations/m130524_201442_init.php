9c8f20c42af35b8a32265d1a6f1a57ff59dab759
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-12-04 17:21:22 +0900

    User migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -9,7 +9,7 @@ class m130524_201442_init extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
 
         $this->createTable('{{%user}}', [
@@ -18,12 +18,25 @@ class m130524_201442_init extends Migration
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
+            'verification_token' => $this->string()->defaultValue(null),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->unsigned()->notNull(),
+            'updated_at' => $this->integer()->unsigned()->notNull(),
         ], $tableOptions);
+
+        $this->insert('{{%user}}', [
+            'id' => 1,
+            'username' => 'admin',
+            'auth_key' => 'NcBMXcpL2fU9w5oJXB0crexMjRMPAXd0',
+            'password_hash' => '$2y$13$RM/hpBAaqBLDbpLzhiZyfuFQrVh5MXnhFPVQVMDdjEC4395tMzuX.',
+            'password_reset_token' => null,
+            'verification_token' => 'xAvagpK9pe6xufQhT2gCTq58i-l7kJ2P_1606138472',
+            'email' => 'admin@gmail.com',
+            'status' => 10,
+            'created_at' => 1606138472,
+            'updated_at' => 1606138472
+        ]);
     }
 
     public function down()
